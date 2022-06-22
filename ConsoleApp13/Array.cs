465443043cc0bb457f3965b4373391ecce29d3e7
Goran Vukojičić <goranvukojicic@gmail.com>
2022-06-22 14:01:30 +0200

    Add project files.

---
+++ ConsoleApp13/Array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    public class Array
+    {
+        public static bool containsDuplicate(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return false;
+
+            for (int i=0;i<nums.Length;i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j]) 
+                    { 
+                        return true; 
+                    }
+                    
+                }
+            }
+            return false;  
+        }
+    }
+}
---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 4, 2, 3, 4 };
+           
+            Console.WriteLine(Array.containsDuplicate(nums));
+        }
+    }
+}
